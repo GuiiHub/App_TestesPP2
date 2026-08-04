4d64d5043d362292189f7fd036660a151e658df1
Guilherme Queiroz <guilherme.queiroz@academico.ufpb.br>
2026-08-04 20:30:13 -0300

    fix: update subheader wording for displaying CSV data in Streamlit app

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ nome = st.text_input("Digite seu nome:")
 if nome:
     st.success(f"Olá, {nome}, seja bem-vindo(a)! O app Streamlit está funcionando perfeitamente.")
 
-st.subheader("Exibindo dados em formato CSV")
-
+st.subheader("Exibindo dataframe em formato CSV")
 import pandas as pd
 
 data = r"C:\UF\Prog UFPB\Atividades PP II\App_TestesPP2\data\info.csv"
