67fd672146910b3047eb9080bec7f3de6ebdba0c
Guilherme Queiroz <guilherme.queiroz@academico.ufpb.br>
2026-08-04 20:25:29 -0300

    feat: add subheader for displaying CSV data in Streamlit app

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ nome = st.text_input("Digite seu nome:")
 if nome:
     st.success(f"Olá, {nome}, seja bem-vindo(a)! O app Streamlit está funcionando perfeitamente.")
 
+st.subheader("Exibindo dados em formato CSV")
+
 import pandas as pd
 
 data = r"C:\UF\Prog UFPB\Atividades PP II\App_TestesPP2\data\info.csv"
