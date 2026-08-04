33c59dd2c6bc22d69f3aca22f9a241b0fb1888a6
Guilherme Queiroz <guilherme.queiroz@academico.ufpb.br>
2026-08-04 20:11:38 -0300

    feat: add data loading and display functionality in Streamlit app

--- app.py
+++ app.py
@@ -8,4 +8,10 @@ nome = st.text_input("Digite seu nome:")
 if nome:
     st.success(f"Olá, {nome}, seja bem-vindo(a)! O app Streamlit está funcionando perfeitamente.")
 
+import pandas as pd
+
+data = r"C:\UF\Prog UFPB\Atividades PP II\App_TestesPP2\data\info.csv"
+
+df = pd.read_csv(data)
+st.dataframe(df)
 
