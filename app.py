a6088d7ddbe59b9a958fc50e8d1788e282f6c675
Guilherme Queiroz <guilherme.queiroz@academico.ufpb.br>
2026-08-04 22:32:38 -0300

    fix: update greeting message format for better readability

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.subheader("Exemplo simples de aplicação web usando Streamlit.")
 nome = st.text_input("Digite seu nome/usuário:")
 
 if nome:
-    st.success(f"Olá, {nome}, seja bem-vindo(a)! O app Streamlit está funcionando perfeitamente.")
+    st.success(f"Olá, {nome}! Seja bem-vindo(a)! O app Streamlit está funcionando perfeitamente.")
     st.subheader("Exibindo dataframe em formato CSV")
     import pandas as pd
 
